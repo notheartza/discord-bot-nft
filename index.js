4c5c33320e55a8641877c4f08610ad4528fbde8c
notheartza <not.102@hotmail.com>
2021-08-23 18:23:47 +0700

    add

--- index.js
+++ index.js
@@ -9,35 +9,52 @@ const cryptocurrency = axios.create({
   headers: { 'X-CMC_PRO_API_KEY': '2e4fc99b-3658-4b86-805c-48f78d2c9ef9' }
 })
 
+list = [
+  'USDT',
+  'BTC',
+  'ETH',
+  'WETH',
+  'BNB',
+  'WAXP',
+  'TLM',
+  'SLP',
+  'AXS',
+  'PVU',
+  'SAND']
+
+  // 'DINOP',
+
+coin = []
+text = ''
+
 
 bot.login(process.env.TOKEN);
 bot.on("ready", () => {
   console.log('bot Ready!')
 })
 
-cron.schedule('0 0 * * * *', async () => {
+cron.schedule('59 * * * * *', async () => {
   console.log('start coin');
   var ts = new Date()
   const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-  var time = ts.toLocaleTimeString("en-US", { timeZone: "Asia/Bangkok",  hour12: false  });
-  var day = ts.toLocaleDateString("en-US", { timeZone: "Asia/Bangkok", options});
-  cryptocurrency.get('/cryptocurrency/listings/latest?convert=THB',
+  var time = ts.toLocaleTimeString("en-US", { timeZone: "Asia/Bangkok", hour12: false });
+  var day = ts.toLocaleDateString("en-US", { timeZone: "Asia/Bangkok", options });
+  cryptocurrency.get('/cryptocurrency/listings/latest?start=1&limit=5000&convert=THB',
     {
       json: true,
       gzip: true
     }).then((result) => {
-      let index_btc = result.data.data.findIndex(x => x.symbol === 'BTC')
-      let index_bnb = result.data.data.findIndex(x => x.symbol === 'BNB')
-      let btc = result.data.data[index_btc]
-      let bnb = result.data.data[index_bnb]
-      console.log(btc)
+      list.map(name => {
+        coin.push(result.data.data.find(x => x.symbol === name))
+      })
+      coin.forEach(data=> text+=textAnnouce(data.symbol, data.quote.THB.price, 'THB'));
       bot.guilds.cache.forEach((guild) => {
         if ('notheartbottester' === guild.name) {
           guild.channels.cache.forEach((room) => {
             if (room.isText()) {
               if (room.name === 'test-announce') {
-                room.send(`ราคาของ ${btc.symbol} อัพเดทวันที่ ${day} ณ เวลา ${time} น. ราคาอยู่ที่ ${btc.quote.THB.price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')} THB`+"\n"+
-                `ราคาของ ${bnb.symbol} อัพเดทวันที่ ${day} ณ เวลา ${time} น. ราคาอยู่ที่ ${bnb.quote.THB.price.toLocaleString(undefined, {minimumFractionDigits: 2, maximumFractionDigits: 2})} THB`)
+                room.send(`อัพเดทราคาของวันที่ ${day} ณ เวลา ${time} น.\n `);
+                room.send(text);
               }
             }
           })
@@ -45,3 +62,8 @@ cron.schedule('0 0 * * * *', async () => {
       })
     })
 }, 'Asia/Bangkok')
+
+
+function textAnnouce(symbol, price, unit) {
+  return `ราคาของ ${symbol} อยู่ที่ ${price.toFixed(4).replace(/\d(?=(\d{3})+\.)/g, '$&,')} ${unit}\n`
+}
